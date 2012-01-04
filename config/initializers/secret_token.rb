e66782be955d59ab994ae010651086ee6d55c863
Max Katz <maxwelljkatz@gmail.com>
2012-01-04 14:53:53 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QlapWeblog::Application.config.secret_token = '82ed332c4bc5949422eef6e534552cd9a0323d7cd3a494e3050eb75473582cbc6bd6601d2cb9ba325fa3237512becf673de2052e2a14d206f403a4533a59e61e'
