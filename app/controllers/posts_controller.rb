c3520777555948b6cf1f377b682c7afd9792d5ce
Max Katz <maxwelljkatz@gmail.com>
2012-01-11 15:37:50 -0700

    Fixed where a successful create action redirects you to, was going to root_path (home action), changed it to posts_path (index action). Created an update action that updates an existing post. If update is successful, it redirects you to posts_path (index action), and if it fails the app rerenders edit_post_path (edit action).

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
 
 	def show
 		@post = Post.find(params[:id])
+		
 	end
 
 	def new
@@ -14,7 +15,7 @@ class PostsController < ApplicationController
 	def create
 		@post = Post.new(params[:post])
 		if @post.save
-			redirect_to root_path
+			redirect_to posts_path
 		else
 			render new_post_path
 		end
@@ -26,5 +27,10 @@ class PostsController < ApplicationController
 
 	def update
 		@post = Post.find(params[:id])
+		if @post.update_attributes(params[:post])
+			redirect_to posts_path
+		else
+			render edit_post_path
+		end
 	end
 end
